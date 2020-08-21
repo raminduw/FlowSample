f39bf0d0e54d87330ec3670b7f996d7574188699
Ramindu Weeraman <ramindu.weeraman@gmail.com>
2020-08-21 16:08:44 +0200

    Added flow.

--- app/src/main/java/com/ramindu/weeraman/flowsample/app/MainActivity.kt
+++ app/src/main/java/com/ramindu/weeraman/flowsample/app/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.ramindu.weeraman.flowsample.app
 
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.activity.viewModels
@@ -13,7 +14,10 @@ import com.ramindu.weeraman.flowsample.domain.GetBreedListUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 
@@ -31,6 +35,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+    }
+
+    private fun loadData(){
+        dogList.clear()
+        recyclerViewDogs.clear()
 
         lifecycleScope.launchWhenCreated {
             breedViewModel.getAllBreeds().onStart {
@@ -54,6 +65,23 @@ class MainActivity : AppCompatActivity() {
                     name(it)
                 }
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.getItemId()) {
+            R.id.action_update -> {
+                loadData()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
